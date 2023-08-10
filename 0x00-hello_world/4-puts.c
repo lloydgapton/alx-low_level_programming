b5efd8847a6effe705ae5d4fc32d0adf68bcdd0a
lloydgapton <108632761+lloydgapton@users.noreply.github.com>
2023-08-10 16:06:29 +0100

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,18 +2,15 @@
  * This program demonstrates the use of the puts function.
  */
 #include <stdio.h>
-
 int main(void)
 {
 	/**
-         * The main function is the entry point of the program
-         * Its where the execution of the program begins.
-	 */
+ *The main function is the entry point of the program
+ *Its where the execution of the program begins.
+ */
 	puts("\"Programming is like building a multilingual puzzle");
-	
 	return (0);
-	
 	/** 
-         * The return value of 0 indicates successful execution of the program.
-         */
+ *The return value of 0 indicates successful execution of the program.
+ */
 }
