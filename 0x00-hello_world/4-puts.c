332b2d4cf0814a3d03277936afb3e45dd79073b2
lloydgapton <lloydgapton123@gmail.com>
2023-08-10 07:13:10 -0700

    update

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,6 +5,7 @@
 
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
+	puts(" ");
 	return(0);
-}
+};
