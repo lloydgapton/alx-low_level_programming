3f83b0c042aada6fa653f74d4b7398390c6a09e4
lloydgapton <108632761+lloydgapton@users.noreply.github.com>
2023-08-10 21:17:45 +0100

    Create 5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Using printf to print the desired message 
+ * with proper grammar.
+(* section header: Section description)*
+ * Return: Description of the returned value
+ */
+int main() {
+    printf("with proper grammar, but the outcome is a piece of art,\n");
+  /* End the program with the value 0 */
+  return 0;
+}
