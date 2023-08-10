6cf7f9de14910df5bf4aa63e907200c86d95a7b8
lloydgapton <108632761+lloydgapton@users.noreply.github.com>
2023-08-10 21:03:59 +0100

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,6 @@
 #include <stdio.h>
 /**
  * main - Short description, single line
-(* a blank line
- * Description: Longer description of the function)?
-(* section header: Section description)*
  * Return: Description of the returned value
  */
 int main(void)
