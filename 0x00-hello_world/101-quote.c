e853bb55c79100c4f86f135b9064a4e326207320
lloydgapton <lloydgapton123@gmail.com>
2023-08-10 15:39:49 -0700

    update

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,7 +7,8 @@
  */
 int main(void)
 {
-	const char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	const char *message =
+		"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 	write(2, message, strlen(message));
 	return (1);
 }
