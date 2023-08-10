b8486c257e7ad7e08a39aaf5cc2480036823f0bc
lloydgapton <108632761+lloydgapton@users.noreply.github.com>
2023-08-10 19:37:43 +0100

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -12,5 +12,4 @@ int main(void)
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 	/* Return: Description of the returned value */
-	/*The return value of 0 indicates successful execution of the program.*/	
 }
