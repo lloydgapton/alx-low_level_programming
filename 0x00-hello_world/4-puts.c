49933b5ae3adeaf6c0a76b399a2fb97e19ba3664
lloydgapton <108632761+lloydgapton@users.noreply.github.com>
2023-08-10 20:00:58 +0100

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 /**
  * main - Short description, single line
- * @parameterx: Description of parameter x
 (* a blank line
  * Description: Longer description of the function)?
 (* section header: Section description)*
