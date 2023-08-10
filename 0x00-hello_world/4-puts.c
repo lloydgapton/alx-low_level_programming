7b0f1d96aa4f7ddcbac3e04d739b892bd097ecbe
lloydgapton <108632761+lloydgapton@users.noreply.github.com>
2023-08-10 19:56:09 +0100

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,12 @@
 #include <stdio.h>
+/**
+ * function_name - Short description, single line
+ * @parameterx: Description of parameter x
+(* a blank line
+ * Description: Longer description of the function)?
+(* section header: Section description)*
+ * Return: Description of the returned value
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
