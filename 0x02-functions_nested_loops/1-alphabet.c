d723c9addcac39b5a4c6080f2574ff5fd0dcdfc8
lloydgapton <lloydgapton123@gmail.com>
2023-08-15 07:15:02 -0700

    update

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -6,11 +6,10 @@
 
 void print_alphabet(void)
 {
-	char letter = 'a';
-	for(letter = 'a'; letter ='z'; letter ++)
+	char letter;
+	for(letter == 'a'; letter =='z'; letter ++)
 	{
 	       	_putchar(letter);
 	}
 	_putchar('\n');
-	return (0);
 }
