7d16751c6d72399cefa30dc7dd8d949f6a302621
lloydgapton <lloydgapton123@gmail.com>
2023-08-15 07:22:48 -0700

    update

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,7 +1,6 @@
 #include "main.h"
 /**
  * main - print lowercase alphabets
- * Return : (0)
  */
 
 void print_alphabet(void)
@@ -9,7 +8,7 @@ void print_alphabet(void)
 	char letter;
 	for(letter ='a'; letter <='z'; letter++)
 	{
-	       	_putchar(letter);
+		_putchar(letter);
 	}
 	_putchar('\n');
 }
