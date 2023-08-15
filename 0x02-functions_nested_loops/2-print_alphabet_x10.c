fe0523d4d66b5dd3e70d4a489603271aa42039ef
lloydgapton <lloydgapton123@gmail.com>
2023-08-15 08:00:34 -0700

    update

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,14 @@
+#include "main"
+/**
+ * print_alphabet_x10 - print 10 times the alphabet, in lowercase
+ */
+void print_alphabet_x10(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		_putchar(letter)
+	}
+	_putchar 
+}
