404f03973518236d14346c10ceebdce1eac79161
lloydgapton <108632761+lloydgapton@users.noreply.github.com>
2023-08-15 11:12:02 +0100

    Create 11-print_to_98.c

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+void print_to_98(int n) {
+    for (; n <= 98; n++) {
+        printf("%d", n);
+        if (n != 98) {
+            printf(", ");
+        }
+    }
+    printf("\n");
+}
+
+int main() {
+    int start;
+    printf("Enter a starting number: ");
+    scanf("%d", &start);
+
+    print_to_98(start);
+
+    return 0;
+}
