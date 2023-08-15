3b8a130a019a1d12b709ea2914d35f44aed639cc
lloydgapton <108632761+lloydgapton@users.noreply.github.com>
2023-08-15 10:57:24 +0100

    Create 3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,7 @@
+int _islower(int c) {
+    if (c >= 'a' && c <= 'z') {
+        return 1;  // Return 1 if c is lowercase
+    } else {
+        return 0;  // Return 0 if c is not lowercase
+    }
+}
