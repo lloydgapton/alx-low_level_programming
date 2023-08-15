9e8ce70758e6c29a7320aa71fd3a7e787225ba15
lloydgapton <108632761+lloydgapton@users.noreply.github.com>
2023-08-15 23:54:20 +0100

    Update 104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -39,16 +39,12 @@ int main(void)
 	printf("\n");
 	return (0);
 }
-
-
-
 _putchar.c
 #include "main.h"
 #include <unistd.h>
 /**
  * _putchar - writes the character c to stdout
  * @c: The character to print
- *
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
