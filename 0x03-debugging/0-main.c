8cda34f4a1f429a62e938e1490b9be8ef3b398ec
lloydgapton <lloydgapton123@gmail.com>
2023-08-16 05:17:13 -0700

    update

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -7,11 +7,11 @@
 
 int main(void)
 {
-        int i;
- 
-        i = 0;
- 
-        positive_or_negative(i);
+	int i;
 
-        return (0);
+	i = 0;
+
+	positive_or_negative(i);
+
+	return (0);
 }
