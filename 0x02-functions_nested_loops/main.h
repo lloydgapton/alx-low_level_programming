4c2a55cbaec14d8605f26fe6211769ee784f3b6c
lloydgapton <108632761+lloydgapton@users.noreply.github.com>
2023-08-16 13:06:36 +0100

    Update main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,5 +1,6 @@
-#ifndef main_h
-#define main_h
+#ifndef HEADER_FILE
+#define HEADER_FILE
+
 int _putchar(char);
 void print_alphabet(void);
 void print_alphabet_x10(void);
@@ -13,4 +14,5 @@ void times_table(void);
 int add(int, int);
 void print_to_98(int n);
 void print_times_table(int n);
+
 #endif
