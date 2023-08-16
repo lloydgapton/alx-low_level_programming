bccaab8b4f3cad532d3fd1ee63c852a24dfb268f
lloydgapton <lloydgapton123@gmail.com>
2023-08-16 05:25:27 -0700

    update

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -14,11 +14,10 @@ int main(void)
         i = 0;
 
         while (i < 10)
-        {
-               i++; /* putchar(i); */
-        }
+	{
+		i++; /* putchar(i); */
+	}
+	printf("Infinite loop avoided! \\o/\n");
 
-        printf("Infinite loop avoided! \\o/\n");
-
-        return (0);
+	return (0);
 }
