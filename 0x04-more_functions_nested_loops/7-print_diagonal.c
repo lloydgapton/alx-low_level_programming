acc9408827613e2260f3cbe513fcef1e65444b23
lloydgapton <108632761+lloydgapton@users.noreply.github.com>
2023-08-18 06:23:21 +0100

    Create 7-print_diagonal.c

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * print_diagonal - function that draws a diagonal line on the terminal
+ *
+ * @n: this is the size of the diagonal
+ *
+ * Return: There is no error
+ **/
+
+void print_diagonal(int n)
+{
+	int replay, count;
+
+	for (replay = 1; n > 0 && replay <= n; replay++)
+	{
+		for (count = 1; count <= replay; count++)
+		{
+			if (count == replay)
+			{
+				_putchar(92);
+			}
+			else
+			{
+				_putchar(32);
+			}
+		}
+		_putchar('\n');
+	}
+	if (n <= 0)
+		_putchar('\n');
+}
