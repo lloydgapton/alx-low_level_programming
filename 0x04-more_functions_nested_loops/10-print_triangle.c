96a08381d4e52ce1e45fc4e7435abc4ad4215e64
lloydgapton <108632761+lloydgapton@users.noreply.github.com>
2023-08-18 06:26:17 +0100

    Create 10-print_triangle.c

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_triangle - Write a function that prints a triangle, followed by a new
+ * line.
+ * @size: demensions of triangle
+ * Return: nothing
+ **/
+
+void print_triangle(int size)
+{
+	int replay, count;
+
+	if (size < 1)
+	{
+		_putchar('\n');
+		return;
+	}
+	for (replay = 1; replay <= size; replay++)
+	{
+		for (count = size - replay; count > 0; count--)
+			_putchar(' ');
+		for (count = replay; count > 0; count--)
+			_putchar('#');
+		_putchar('\n');
+	}
+}
