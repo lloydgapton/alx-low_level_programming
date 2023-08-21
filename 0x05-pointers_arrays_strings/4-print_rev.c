68146cb9d5df78516dcef37a5fe4871b92a7aa16
lloydgapton <lloydgapton123@gmail.com>
2023-08-21 14:31:59 -0700

    update

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -14,9 +14,9 @@ void print_rev(char *s)
 	{
 		length++;
 	}
-	for (int i = length -1; i >= 0; i--)
+	for (length = length -1; s[length] != '\0'; length--)
 	{
-		putchar(s[i]);
+		putchar(s[length]);
 	}
 
 	putchar('\n');
