67e53505a476a330c5e01e8d5a045f1be0a7b2c8
lloydgapton <108632761+lloydgapton@users.noreply.github.com>
2023-08-22 00:26:40 +0100

    Create 9-strcpy.c

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strcpy - convers a string to an integer
+ * @dest: This is destiny
+ * @src: This is the copia
+ *
+ * Return: This return copy
+ */
+char *_strcpy(char *dest, char *src)
+{
+	char *start = dest;
+
+	while (*src != '\0')
+	{
+		*dest = *src;
+		dest++;
+		src++;
+	}
+	*dest = '\0';
+	return (start);
+}
