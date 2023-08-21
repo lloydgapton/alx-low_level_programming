ecc0cf3057bb4a787bef70a222daa39010bea957
lloydgapton <lloydgapton123@gmail.com>
2023-08-21 16:09:14 -0700

    update

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -12,20 +12,21 @@ void puts_half(char *str)
 	int length = strlen(str);
 	int start_index;
 
-	if (length % 2 == 1)
-	{
-		start_index = length / 2;
-	}
-	else
-	{
-		start_index = (length - 1) / 2;
-	}
+	length = 0;
 
 	while (str[start_index] != '\0')
 	{
-		putchar(str[start_index]);
+		length++;
+	}
+	start_index = length / 2;
+
+	if (length % 2 == 1)
+		start_index++;
+
+	while(start_index < length)
+	{
+		_putchar(str[start_index]);
 		start_index++;
 	}
-
-	putchar('\n');
+	_putchar('\n');
 }
