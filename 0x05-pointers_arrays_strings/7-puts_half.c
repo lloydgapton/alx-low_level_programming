6ffe4a86fad8e33ad0b63deb055958a08ccb0c22
lloydgapton <lloydgapton123@gmail.com>
2023-08-21 15:39:41 -0700

    write a file that prints half of a string

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,27 @@
+/**
+ * puts_half - prints half of a string
+ * @str: this ais an entry
+ * Return Always 0
+ */
+
+#include "main.h"
+#include <string.h>
+
+void puts_half(char 8str)
+{
+	int length = strlen(str);
+	int start_index;
+
+	if (length % 2 == 0)
+	{
+		start_index =length / 2;
+	}
+
+	while (str[start_index] != '\0')
+	{
+		putchar(str[start_indxe]);
+		start_index++;
+	}
+
+	putchar('\n');
+}
