50eb34f0d5250c0fcd0385f6999f59ec531e9241
lloydgapton <lloydgapton123@gmail.com>
2023-08-21 14:36:43 -0700

    update

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,4 +1,5 @@
-/**print_rev - prints a string in reverse.
+/**
+ * print_rev - prints a string in reverse.
  * @s: this is the entry
  * Return : Always 0
  */
@@ -10,11 +11,11 @@ void print_rev(char *s)
 {
 	int length = 0;
 
-	while(s[length] != '\0')
+	while (s[length] != '\0')
 	{
 		length++;
 	}
-	for (length = length -1; s[length] != '\0'; length--)
+	for (length = length - 1; s[length] != '\0'; length--)
 	{
 		putchar(s[length]);
 	}
