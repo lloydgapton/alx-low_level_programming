5fb9f1d6e92b3519b50bc1e31a77ddde154e77d4
lloydgapton <lloydgapton123@gmail.com>
2023-08-21 17:08:22 -0700

    update file

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,1 +1,25 @@
+/**
+ * generate_password -it generates randome passwords
+ * @length: this is an entry
+ * @password: this is the second entry.
+ */
 
+#include <stdio.h>
+#include "main.h"
+#include <stdlib.h>
+#include <time.h>
+
+void generate_password(char *password, int length)
+{
+	const char valid_chars[] = "abcdefghijklmnopqrstuvwxyxABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+
+	srand(time(NULL));
+
+	for (int i = 0; i < length; i++)
+	{
+		int random_inde = rand() % (sizeof(valid_chars) - 1);
+		password[i] = valid_chars[random_index];
+	}
+
+	password[length] = '\0';
+}
