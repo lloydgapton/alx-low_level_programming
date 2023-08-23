94b1f6a68102b3396d95f5cc978ac1ff732ce557
lloydgapton <lloydgapton123@gmail.com>
2023-08-23 02:18:41 -0700

    added new files

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,44 @@
+/**
+ * *_strcat - conactenates two strings
+ * @*dest :this is the first entry.
+ * @*src :this is the second entry
+ * Return :Always 0
+ */
+
+#include <unistd.h>
+#include "main.h"
+
+char *_strcat(char *dest, char *src)
+{
+	char *result = dest;
+
+	while(*dest != '\0')
+	{
+		dest++;
+	}
+
+	while (*src != '\0')
+	{
+		*dest = *src;
+		dest++;
+		src++;
+	}
+
+	*dest = '\0';
+
+	return (result);
+}
+
+void _putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void_puts(char *str)
+{
+	while(*str != '\0')
+	{
+		_putchar(*str);
+		str++;
+	}
+}
