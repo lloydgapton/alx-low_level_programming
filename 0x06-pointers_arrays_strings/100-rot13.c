f12463d3cf076cfcdb85c76fe980d9580af92c92
lloydgapton <lloydgapton123@gmail.com>
2023-08-24 07:29:53 -0700

    update

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,17 +1,21 @@
 /**
  * rot13 - encodes a string using rot 13.
- * @s : this is an entry
- * Return : rot13.
+ * @s: this is an entry
+ * Return: address of s.
  */
 
 char *rot13(char *s)
 {
 	char *p = s;
-	
-	while (*p) {
-        if (*p >= 'a' && *p <= 'z') {
-            *p = (*p - 'a' + 13) % 26 + 'a';
-        } else if (*p >= 'A' && *p <= 'Z') {
+
+	while (*p)
+	{
+        if(*p >= 'a' && *p <= 'z')
+	{
+		*p = (*p - 'a' + 13) % 26 + 'a';
+	}
+	else if(*p >= 'A' && *p <= 'Z')
+	{
             *p = (*p - 'A' + 13) % 26 + 'A';
         }
         p++;
