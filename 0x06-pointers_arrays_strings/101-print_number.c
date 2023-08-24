180e86f3e5525d95f3eed1d90e1e1e7d02948559
lloydgapton <lloydgapton123@gmail.com>
2023-08-24 09:20:36 -0700

    update

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -8,12 +8,14 @@
 
 void print_number(int n)
 {
-    if (n < 0) {
-        _putchar('-');
-        n = -n;
-    }
-    if (n / 10) {
-        print_number(n / 10);
-    }
-    _putchar(n % 10 + '0');
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+	if (n / 10)
+	{
+		print_number(n / 10);
+	}
+	_putchar(n % 10 + '0');
 }
