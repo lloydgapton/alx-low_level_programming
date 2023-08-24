dcdc1219d79a529ff28f733216a207ddff3b4850
lloydgapton <108632761+lloydgapton@users.noreply.github.com>
2023-08-24 16:57:54 +0100

    Update 103-infinite_add.c

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,47 +1,47 @@
+#include "main.h"
+
+/**
+ * infinite_add - adds two numbers
+ * @n1: first number
+ * @n2: second number
+ * @r: buffer for result
+ * @size_r: buffer size
+ *
+ * Return: address of r or 0
+ */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-    int len1 = 0;
-    while (n1[len1]) {
-        len1++;
-    }
-    int len2 = 0;
-    while (n2[len2]) {
-        len2++;
-    }
-    int len = len1 > len2 ? len1 : len2;
-    if (len + 1 > size_r) {
-        return 0;
-    }
-    int carry = 0;
-    int i = len1 - 1;
-    int j = len2 - 1;
-    int k = len;
-    r[k] = '\0';
-    while (i >= 0 || j >= 0) {
-        int sum = carry;
-        if (i >= 0) {
-            sum += n1[i] - '0';
-            i--;
-        }
-        if (j >= 0) {
-            sum += n2[j] - '0';
-            j--;
-        }
-        if (sum >= 10) {
-            carry = 1;
-            sum -= 10;
-        } else {
-            carry = 0;
-        }
-        k--;
-        r[k] = sum + '0';
-    }
-    if (carry == 1) {
-        k--;
-        if (k < 0) {
-            return 0;
-        }
-        r[k] = '1';
-    }
-    return &r[k];
+	int i, j, k, l, m, n;
+
+	for (i = 0; n1[i]; i++)
+		;
+	for (j = 0; n2[j]; j++)
+		;
+	if (i > size_r || j > size_r)
+		return (0);
+	m = 0;
+	for (i -= 1, j -= 1, k = 0; k < size_r - 1; i--, j--, k++)
+	{
+		n = m;
+		if (i >= 0)
+			n += n1[i] - '0';
+		if (j >= 0)
+			n += n2[j] - '0';
+		if (i < 0 && j < 0 && n == 0)
+		{
+			break;
+		}
+		m = n / 10;
+		r[k] = n % 10 + '0';
+	}
+	r[k] = '\0';
+	if (i >= 0 || j >= 0 || m)
+		return (0);
+	for (k -= 1, l = 0; l < k; k--, l++)
+	{
+		m = r[k];
+		r[k] = r[l];
+		r[l] = m;
+	}
+	return (r);
 }
