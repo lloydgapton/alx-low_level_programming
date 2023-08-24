0dfdfc38b5f6255a71913999cd773f7ef74643b2
lloydgapton <108632761+lloydgapton@users.noreply.github.com>
2023-08-24 18:19:11 +0100

    Update 101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -5,17 +5,30 @@
  */
 
 #include "main.h"
+#include <unistd.h>
 
-void print_number(int n)
-{
-	if (n < 0)
-	{
-		putchar('-');
-		n = -n;
-	}
-	if (n / 10)
-	{
-		print_number(n / 10);
-	}
-	putchar(n % 10 + '0');
+int _putchar(char c) {
+    return write(1, &c, 1);
 }
+
+void print_number(int n) {
+    int divisor = 1;
+    int temp = n;
+
+    if (n < 0) {
+        _putchar('-');
+        n = -n;
+    }
+
+    while (temp > 9) {
+        divisor *= 10;
+        temp /= 10;
+    }
+
+    while (divisor > 0) {
+        _putchar((n / divisor) + '0');
+        n %= divisor;
+        divisor /= 10;
+    }
+}
+
