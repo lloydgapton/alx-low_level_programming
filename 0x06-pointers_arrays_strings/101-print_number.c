cee7c20a34747fde0ff6413032c3f1b91402a122
lloydgapton <108632761+lloydgapton@users.noreply.github.com>
2023-08-24 19:11:44 +0100

    Update 101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -19,11 +19,23 @@ void print_number(int n)
 
 	_putchar((num % 10) + '0');
 }
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
-    int number = 98;
-    print_number(number);
-
+    print_number(98);
     _putchar('\n');
-    return 0;
+    print_number(402);
+    _putchar('\n');
+    print_number(1024);
+    _putchar('\n');
+    print_number(0);
+    _putchar('\n');
+    print_number(-98);
+    _putchar('\n');
+    return (0);
 }
