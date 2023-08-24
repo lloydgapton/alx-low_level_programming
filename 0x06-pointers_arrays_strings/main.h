80f7dbd2d6ff07b1452271e6903c087e26dc70f0
lloydgapton <108632761+lloydgapton@users.noreply.github.com>
2023-08-24 14:43:52 +0100

    Update main.h

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -8,7 +8,7 @@ char *_strncpy(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
 void reverse_array(int *a, int n);
 char *string_toupper(char *);
-char *cap_string(char *);
+char *cap_string(char *s)
 char *leet(char *);
 char *rot13(char *);
 char *_strstr(char *haystack, char *needle);
