f2b742301fbc454a0816da04d52e585f1ea1e313
lloydgapton <108632761+lloydgapton@users.noreply.github.com>
2023-08-24 12:00:34 +0100

    Create 101-print_number.c

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,11 @@
+void print_number(int n)
+{
+    if (n < 0) {
+        _putchar('-');
+        n = -n;
+    }
+    if (n / 10) {
+        print_number(n / 10);
+    }
+    _putchar(n % 10 + '0');
+}
