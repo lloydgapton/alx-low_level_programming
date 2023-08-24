e102dbda9c57d474c9d0960dd04966a06c09d6e9
lloydgapton <lloydgapton123@gmail.com>
2023-08-24 07:23:10 -0700

    update

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -6,9 +6,9 @@
 
 char *rot13(char *s)
 {
-    char *p = s;
-
-    while (*p) {
+	char *p = s;
+	
+	while (*p) {
         if (*p >= 'a' && *p <= 'z') {
             *p = (*p - 'a' + 13) % 26 + 'a';
         } else if (*p >= 'A' && *p <= 'Z') {
