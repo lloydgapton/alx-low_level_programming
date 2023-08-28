0896fdce53b9f29f66ebb5bcc3c02ba635713b78
lloydgapton <108632761+lloydgapton@users.noreply.github.com>
2023-08-28 22:34:04 +0100

    Update 1-memcpy.c

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -3,4 +3,14 @@
  */
 
 #include "main.h"
-
+char *_memcpy(char *dest, char *src, unsigned int n) {
+    char *original_dest = dest;
+    
+    while (n--) {
+        *dest = *src;
+        dest++;
+        src++;
+    }
+    
+    return (original_dest);
+}
