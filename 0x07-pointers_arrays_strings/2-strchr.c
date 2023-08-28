9f058ca42e5bbdf760a2ea9d568b21438c878756
lloydgapton <lloydgapton123@gmail.com>
2023-08-28 15:03:20 -0700

    update

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,3 +1,10 @@
+/**
+ * _strchr -function that locates a character in a string'
+ * @s: this is the input string.
+ * @c:this is the character.
+ * Return: Return to the charcter found, if not , return a NULL.
+ */
+
 #include "main.h"
 char *_strchr(char *s, char c)
 {
@@ -10,9 +17,9 @@ char *_strchr(char *s, char c)
         s++;
     }
     
-    if (c == '\0') {
-        return (s); // Return pointer to the null terminator if c is '\0'
+    if (c == '\0')
+    {
+	    return (s);
     }
-    
-    return (NULL); // Return NULL if character c is not found
+    return (NULL);
 }
