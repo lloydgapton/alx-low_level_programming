1dbcca85546a13afdc6848136d634156a9966683
lloydgapton <lloydgapton123@gmail.com>
2023-08-28 17:07:18 -0700

    added 5-strstr.c

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,29 @@
+/**
+ * _strstr - locates s substring
+ * @haystack: string in which to check for needle
+ * @needle: substring to find in haystack
+ * Return:pointer of beginning of nedle in haystack or NULL if no match
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	unsigned int i = 0, j = 0;
+
+	while (haystack[i])
+	{
+		while (needle[j] && (haystack[i] == needle[0]))
+		{
+			if (haystack[i + j] == needle[j])
+				j++;
+			else
+				break;
+		}
+		if (needle[j])
+		{
+			i++;
+			j = 0;
+		}
+		else
+			return (haystack + i);
+	}
+	return (0);
+}
