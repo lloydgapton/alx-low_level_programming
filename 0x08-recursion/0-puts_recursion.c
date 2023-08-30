8d5026a0a7c504c434075bdde68ffd1f7804989a
lloydgapton <lloydgapton123@gmail.com>
2023-08-30 15:37:38 -0700

    update

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,39 +1,9 @@
 #include "main.h"
 /**
- * _puts_recursion -write a function that prints a string, followed by a nea line
+ * _puts_recursion -print a string, followed by a new line
  * @s: this is the input string.
  */
- void _puts_recursion(char *s)
-{
-	if (*s == '\0')
-	{
-		_putchar('\n');
-		return;
-	}
-	_putchar(*s);
-	s++;
-	_puts_recursion(s);
-}#include "main.h"
-/**
- * _puts_recursion -write a function that prints a string, followed by a nea line
- * @s: this is the input string.
- */
- void _puts_recursion(char *s)
-{
-	if (*s == '\0')
-	{
-		_putchar('\n');
-		return;
-	}
-	_putchar(*s);
-	s++;
-	_puts_recursion(s);
-}#include "main.h"
-/**
- * _puts_recursion -write a function that prints a string, followed by a nea line
- * @s: this is the input string.
- */
- void _puts_recursion(char *s)
+void _puts_recursion(char *s)
 {
 	if (*s == '\0')
 	{
