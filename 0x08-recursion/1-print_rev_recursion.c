4a24410e8fe5bb34b845615de0225776bf008ce7
lloydgapton <lloydgapton123@gmail.com>
2023-08-30 15:43:04 -0700

    added new file

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _print_rev_recursion - print a string in reverse.
+ * @s: This is my entry
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s)
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+}
