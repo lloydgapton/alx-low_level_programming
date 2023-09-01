af0d9565ebda2ec63e77ca1378dae8778ee5abc7
lloydgapton <lloydgapton123@gmail.com>
2023-09-01 08:46:23 -0700

    created new files

---
+++ 0x09-static_libraries/0-memset.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * _memset - this is my function, copy the number, copy the character to s
+ * @s: this is my entry and return.
+ * @b: this is the character to copy
+ * @n: this is the number of bytes
+ * Return :this is my result and Return S
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+}
---
+++ 0x09-static_libraries/100-atoi.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <limits.h>
+/**
+ * _atoi - convert a string to an integer
+ * @s: pointer to the string to convert
+ *
+ * Return : the integer that was converted
+ */
+int _atoi(char *s)
+{
+}
