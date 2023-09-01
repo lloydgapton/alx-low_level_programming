517ab79f2ea1736ff2721b5cfa03b443cfe0c039
lloydgapton <lloydgapton123@gmail.com>
2023-09-01 10:15:24 -0700

    created new files

---
+++ 0x09-static_libraries/_putchar.c
@@ -0,0 +1,11 @@
+#include <unistd.h>
+/**
+ * _putchar - writes the character c to stdout
+ * @c: the character print
+ *
+ * Return: On success 1
+ * on, error, -1 is returned, and error is set appropriately.
+ */
+int _putchar(char c)
+{
+}
