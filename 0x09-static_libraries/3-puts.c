0dfca3754fbabbdadeb6b4710edc0510696b1d82
lloydgapton <lloydgapton123@gmail.com>
2023-09-03 01:27:44 -0700

    update

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _puts - this is the function.
+ * @str: this is an entry
+ */
+void _puts(char *str)
+{
+}
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
