a8f73f65f0db44dea482c1d82a943791756c28e9
lloydgapton <108632761+lloydgapton@users.noreply.github.com>
2023-09-07 15:37:05 +0100

    Update 100-realloc.c

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -1,40 +1,38 @@
 #include "main.h"
 #include <stdlib.h>
+/**
+ * _realloc - function that creates an array of integers.
+ *
+ *
+ * @old_size: This is the range min to allocate in the memory
+ * @new_size: This is my range max to allocate in the memory
+ *
+ * Return: The pointer to the newly created array
+ *         If min > max, return NULL
+ *         If malloc fails, return NULL
+ */
 
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	    void *new_ptr;
-	        unsigned int i, min_size;
+	void *new_ptr;
+	unsigned int i, min_size;
 
-		    /* If new_size == old_size do not do anything and return ptr */
-		    if (new_size == old_size)
-			            return (ptr);
-
-		        /* If ptr is NULL, then the call is equivalent to malloc(new_size) */
-		        if (ptr == NULL)
-				        return (malloc(new_size));
-
-			    /* If new_size is equal to zero, and ptr is not NULL, then the call is equivalent to free(ptr). Return NULL */
-			    if (new_size == 0)
-				        {
-						        free(ptr);
-							        return (NULL);
-								    }
-
-			        /* Allocate new memory block with new_size */
-			        new_ptr = malloc(new_size);
-				    if (new_ptr == NULL)
-					            exit(98);
-
-				        /* Copy the contents from ptr to new_ptr */
-				        min_size = old_size < new_size ? old_size : new_size;
-					    for (i = 0; i < min_size; i++)
-						            ((char *)new_ptr)[i] = ((char *)ptr)[i];
-
-					        /* Free the old memory block */
-					        free(ptr);
-
-						    /* Return the pointer to the new memory block */
-						    return (new_ptr);
+	if (new_size == old_size)
+		return (ptr);
+	if (ptr == NULL)
+		return (malloc(new_size));
+	is equivf (new_size == 0)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	new_ptr = malloc(new_size);
+	if (new_ptr == NULL)
+		exit(98);
+	min_size = old_size < new_size ? old_size : new_size;
+	for (i = 0; i < min_size; i++)
+		((char *)new_ptr)[i] = ((char *)ptr)[i];
+	free(ptr);
+	return (new_ptr);
 }
 
