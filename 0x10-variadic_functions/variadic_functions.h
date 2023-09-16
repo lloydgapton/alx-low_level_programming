29e7d1454cbb4f324e8ca69e511c91d56a163882
lloydgapton <lloydgapton123@gmail.com>
2023-09-16 09:41:32 -0700

    added new files

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,20 @@
+#ifndef VARIADIC_FUNCTION_H
+#define VARIADIC_FUNCTION_H
+#include <stdarg.h>
+/**
+ *   * struct format - sructure
+ *     *@f: character
+ *       *@func: function
+ *         */
+typedef struct format
+{
+		char *f;
+			void (*func)(va_list vargs);
+} form;
+int _putchar(char c);
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+
+#endif
