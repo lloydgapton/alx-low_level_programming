7181968fa3e4bab72c1d46fab6f2a7d1aa571baf
lloydgapton <lloydgapton123@gmail.com>
2023-09-21 05:22:43 -0700

    updated file

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -4,9 +4,14 @@
  * pre-main - it prints before the main
  * Return: void
  */
+void my_constructor(void) __attribute__((constructor));
 
-void _attribute_((constructor)) pre_main()
+void my_constructor(void)
 {
-	printf("You're beat! and yet, you must allow,\n"
-			"I bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow, \nI bore my house upon my back!\n");
+}
+int main()
+{
+	printf("Inside the main function.\n");
+	return (0);
 }
