80de05869509b6e29d31da809dbefba2fd1bcd02
lloydgapton <lloydgapton123@gmail.com>
2023-09-21 05:50:46 -0700

    update

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
-#include "lists.h"
 /**
- * pre-main - it prints before the main
+ * pre_main - it prints before the main
  * Return: void
  */
 
-void pre_main(void) __attribute__((constructor));
-
-void pre_main(void)
+void __attribute__ ((constructor)) pre_main()
 {
-	printf("You're beat! and yet, you must allow, \nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow, \n"
+			"I bore my house upon my back!\n");
 }
