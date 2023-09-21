d0376f51e0d489c9f2c819c88393ca316086243d
lloydgapton <lloydgapton123@gmail.com>
2023-09-21 05:52:43 -0700

    updated file

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -6,6 +6,6 @@
 
 void __attribute__ ((constructor)) pre_main()
 {
-	printf("You're beat! and yet, you must allow, \n"
+	printf("You're beat! and yet, you must allow,\n"
 			"I bore my house upon my back!\n");
 }
