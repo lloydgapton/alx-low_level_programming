6471dbe4d3cc5eadf826f5c4b11733540764c9ca
lloydgapton <lloydgapton123@gmail.com>
2023-09-21 06:01:33 -0700

    update

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,9 +1,16 @@
-          global    main
-          extern    printf
+section .data
+    hello db "Hello, Holberton",10,0  ; The string to print, followed by a newline and null terminator
 
-          section   .text
-main:                                       ; This is called by the C library startup code
-          mov       rdi, message            ; First integer (or pointer) argument in rdi
-          call      printf                    ; puts(message)
-          ret                               ; Return from main back into C library wrapper
-message:  db        "Hello, Holberton", 0
+section .text
+    global main
+
+extern printf
+
+main:
+    mov rdi, hello  ; Load the address of the string into rdi (1st argument for printf)
+    call printf     ; Call the printf function
+
+    ; Exit the program
+    mov rax, 60     ; syscall number for exit
+    xor rdi, rdi    ; exit status 0
+    syscall
