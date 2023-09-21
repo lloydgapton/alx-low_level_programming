4453d2b308579a61fa723c869e3269b09e18dcef
lloydgapton <lloydgapton123@gmail.com>
2023-09-21 05:55:40 -0700

    updated file

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,11 +1,9 @@
-		global	main
-		extern	printf
+          global    main
+          extern    printf
 
-		section .text
-main:						;This is called by the library
-		mov	rdi, message		;First integer (or pointer) argument in rdi
-						 ; puts(message)
-						;Return from main back into C library wrapper
-		call	printf
-		ret
-message:	db	"Hello, Holberton", 0
+          section   .text
+main:                                       ; This is called by the C library startup code
+          mov       rdi, message            ; First integer (or pointer) argument in rdi
+          call      printf                    ; puts(message)
+          ret                               ; Return from main back into C library wrapper
+message:  db        "Hello, Holberton", 0
