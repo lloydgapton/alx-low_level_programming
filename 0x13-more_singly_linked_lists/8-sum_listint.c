754d5bd229d7a0a9854facf1c65c8cb656f9d40c
lloydgapton <lloydgapton123@gmail.com>
2023-09-25 16:33:14 -0700

    updated file

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * sum_listint -sum list
+ * @head: head
+ * Return: always 0
+ */
+int sum_listint(listint_t *head)
+{
+	unsigned int sum = 0;
+
+	while (head)
+	{
+		sum += head->n;
+		head = head->next;
+	}
+	return (sum);
+}
---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,46 @@
+#include "lists.h"
+/**
+ * insert_nodeint_at_index - new node position
+ * @head: head
+ * @idx:index of the list
+ * @n: integer
+ * Return: nodes
+ */
+listrint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	unsigned int i = 0;
+	listint_t *node, *node_temporal;
+
+	if (!head && !*head)
+		return (NULL);
+	node_temporal = *head;
+	if (idx == 1)
+	{
+		node = malloc(sizeof(listint_t));
+		if (!node)
+			return (NULL);
+		node->n = n;
+		node->next = *head;
+		*head = node;
+		return (node);
+	}
+	else
+	{
+		while (node_temporal)
+		{
+			if (idx == i + 1)
+			{
+				node = malloc(sizeof(listint_t));
+				if (!node)
+					return (NULL);
+				NODE->n = n;
+				node->next = node_temporal->next;
+				node_temporal->next = node;
+				return (node);
+			}
+			node_temporal = node_temporal->next;
+			i++;
+		}
+	}
+	return (NULL);
+}
