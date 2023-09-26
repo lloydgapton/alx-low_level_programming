a82bc454ecb5d0ee6028e48ba3c2c79b1874be6a
lloydgapton <lloydgapton123@gmail.com>
2023-09-26 15:07:38 -0700

    updates

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -13,7 +13,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	if (head == NULL)
 		return (NULL);
-	for (ptr = *head; ct && ptr != NULL; ct, ptr = ptr->next)
+	for (ptr = *head; ct && ptr != NULL; ct --, ptr = ptr->next)
 		prev = ptr;
 	if (ct)
 		return (NULL);
