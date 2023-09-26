d551c333559e5d3f29fc6d5a4f4e607021cbea0c
lloydgapton <lloydgapton123@gmail.com>
2023-09-25 17:07:33 -0700

    updated code

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -12,8 +12,7 @@ typedef struct listint_t
 {
 	int n;
 	struct listint_t *next;
-}
-listint_t;
+} listint_t;
 int _putchar(char c);
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
