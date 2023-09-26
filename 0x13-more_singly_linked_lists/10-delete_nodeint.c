df188faaf02f7fbaf90c1f7f2079d50415acb781
lloydgapton <lloydgapton123@gmail.com>
2023-09-25 18:10:01 -0700

    updated file

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -7,30 +7,22 @@
  */
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
-	listint_t *prev;
-	listint_t *nxt;
-	unsigned int i;
+	listint_t *ptr, *prev;
 
 	if (head == NULL || *head == NULL)
 		return (-1);
-	nxt = *head;
-	prev = NULL;
 	if (index == 0)
 	{
-		*head = (*head)->next;
-		free(nxt);
+		ptr = (*head)->next;
+		free(*head);
+		*head = ptr;
 		return (1);
 	}
-	for (i = 0; i < index; i += 1)
-	{
-		if (nxt == NULL)
-			return (-1);
-		prev = nxt;
-		nxt = nxt->next;
-	}
-	if (prev)
-		prev->next = nxt->next;
-	free(nxt);
+	for (ptr = *head; index && ptr->next != NULL; index--, ptr = ptr->next)
+		prev = ptr;
+	if (index)
+		return (-1);
+	prev->next = prev->next->next;
+	free(ptr);
 	return (1);
 }
-
