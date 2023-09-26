475f940062a55b14beb51b25616a082589697147
lloydgapton <lloydgapton123@gmail.com>
2023-09-25 17:27:28 -0700

    updated file

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -18,34 +18,3 @@ size_t print_listint(const listint_t *h)
 	return (i);
 }
 
-#include <stdlib.h>
-#include <string.h>
-#include "lists.h"
-#include <stdio.h>
-/**
- * main - check the code
- * Return: always 0
- */
-int main(void)
-{
-	listint_t *head;
-	listint_t *new;
-	listint_t hello = {8, NULL};
-	size_t n;
-
-	head = &hello;
-	new = malloc(sizeof(listint_t));
-	if (new == NULL)
-	{
-		printf("Error\n");
-		return (1);
-	}
-	new->n = 9;
-	new->next = head;
-	head = new;
-	n = print_listint(head);
-	printf("-> %lu elements\n", n);
-	free(new);
-	return (0);
-}
-
