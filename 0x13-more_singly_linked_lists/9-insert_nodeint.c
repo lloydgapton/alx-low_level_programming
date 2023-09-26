eb2b58302bf9c70d647ecd071b0a248057b4b765
lloydgapton <lloydgapton123@gmail.com>
2023-09-26 14:58:43 -0700

    update

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -8,39 +8,33 @@
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	unsigned int i = 0;
-	listint_t *node, *node_temporal;
+	listint_t *ptr, *prev = NULL;
+	int ct = idx;
 
-	if (!head && !*head)
+	if (head == NULL)
 		return (NULL);
-	node_temporal = *head;
-	if (idx == 1)
+	for (ptr = *head; ct && ptr != NULL; ct, ptr = ptr->next)
+		prev = ptr;
+	if (ct)
+		return (NULL);
+	ptr = malloc(sizeof(listint_t));
+	if (ptr == NULL)
+		return (NULL);
+	if (prev != NULL)
 	{
-		node = malloc(sizeof(listint_t));
-		if (!node)
-			return (NULL);
-		node->n = n;
-		node->next = *head;
-		*head = node;
-		return (node);
+		ptr->next = prev->next;
+		prev->next = ptr;
 	}
 	else
 	{
-		while (node_temporal)
-		{
-			if (idx == i + 1)
-			{
-				node = malloc(sizeof(listint_t));
-				if (!node)
-					return (NULL);
-				node->n = n;
-				node->next = node_temporal->next;
-				node_temporal->next = node;
-				return (node);
-			}
-			node_temporal = node_temporal->next;
-			i++;
-		}
+		ptr->next = NULL;
 	}
-	return (NULL);
+	if (*head == NULL || idx == 0)
+	{
+		ptr->next = *head;
+		*head = ptr;
+	}
+	ptr-> = n;
+	return (ptr);
 }
+
