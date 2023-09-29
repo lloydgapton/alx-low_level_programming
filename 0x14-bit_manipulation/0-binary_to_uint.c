f52abe1fc59a79b78937ca393eb69ebcb044f137
lloydgapton <lloydgapton123@gmail.com>
2023-09-29 14:20:52 -0700

    updated file

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * binary_to_unit - binary to int
+ * binary_to_uint - binary to int
  * @b: binary
  * Return: number
  */
@@ -8,6 +8,7 @@
 unsigned int binary_to_uint(const char *b)
 {
 	unsigned int bit = 0;
+
 	while (b && *b)
 	{
 		if (*b != '0' && *b != '1')
--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -8,12 +8,13 @@
 int set_bit(unsigned long int *n, unsigned int index)
 {
 	unsigned long int start = 1;
-	if ((sizeof(unsigned long int) * 8) -1 < index)
+
+	if ((sizeof(unsigned long int) * 8) - 1 < index)
 	{
 		return (-1);
 	}
 	start <<= index;
 	*n = *n | start;
-	
+
 	return (1);
 }
