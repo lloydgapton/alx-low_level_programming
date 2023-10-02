4b3febcf2eb6c2c57c5270d621fb31bc5842ead6
lloydgapton <lloydgapton123@gmail.com>
2023-10-02 13:42:12 -0700

    update 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -6,7 +6,7 @@
  * Return: -1 on failure 1 on success
  */
 
-int create_file(conts char *filename, char *text_content)
+int create_file(const char *filename, char *text_content)
 {
 	int fd, nlet, rwr;
 
