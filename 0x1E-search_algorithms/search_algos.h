8470c27d0285490e04c2082319920c4d64e2cab0
lloydgapton <lloydgapton123@gmail.com>
2024-03-11 23:45:25 -0700

    update

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -22,19 +22,25 @@ typedef struct listint_s
     size_t index;
     struct listint_s *next;
 } listint_t;
-/*-------------Prototype About THe linked List------------------*/
-listint_t *create_list(int *array, size_t size);
+
+typedef struct skiplist_s
+{
+				int n;
+				size_t index;
+				struct skiplist_s *next;
+				struct skiplist_s *express;
+} skiplist_t;
+
 void print_list(const listint_t *list);
 void free_list(listint_t *list);
-listint_t *jump_list(listint_t *list, size_t size, int value);
 
-/* ------------Prototype About The Functions-----------------*/
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 int jump_search(int *array, size_t size, int value);
 int interpolation_search(int *array, size_t size, int value);
 int exponential_search(int *array, size_t size, int value);
 int advanced_binary(int *array, size_t size, int value);
+listint_t *jump_list(listint_t *list, size_t size, int value);
 skiplist_t *linear_skip(skiplist_t *list, int value);
 
 #endif
