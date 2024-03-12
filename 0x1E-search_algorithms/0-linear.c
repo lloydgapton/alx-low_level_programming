bc96168ea44412d0a4b574decdfea6983d566252
lloydgapton <lloydgapton123@gmail.com>
2024-03-11 23:05:55 -0700

    update

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -11,14 +11,11 @@ int linear_search(int *array, size_t size, int value)
 {
 	size_t i;
 
-	if (array == NULL)
-		return (-1);
-
 	for (i = 0; i < size; i++)
 	{
-		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
 
-		if (array[i] == value)
+		printf("Value checked array[%lu] = [%i]\n", i, array[i]);
+		if (value == array[i])
 			return (i);
 	}
 	return (-1);
